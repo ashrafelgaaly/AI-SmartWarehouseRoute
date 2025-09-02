1b4a09ecd03cbaeeb70673a3287d2447857895f8
Ashraf <ash.gaaly@gmail.com>
2025-09-02 11:15:43 -0400

    Final

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from route_generator import generate_rte
 from Plotting import plot_route
 
 if __name__ == "__main__":
-    warehouse_size = (10, 10)
-    start = (7, 7)
-    order = [(2, 2), (4, 1), (1, 5), (6, 4), (9, 3)]
+    warehouse_size = (5, 5)
+    start = (2, 3)
+    order = [(2, 2), (4, 1), (3, 3), (1, 1)]
 
     for policy in ["simple", "greedy", "tsp", "q-learning"]:
         time, dist, route = generate_rte(order, policy=policy, start=start, episodes=500)
